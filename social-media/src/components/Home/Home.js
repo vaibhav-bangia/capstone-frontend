5eba99a77e0fdd14ffc50f0066719ca9bf60957e
KanishkaSoni <54025453+KanishkaSoni@users.noreply.github.com>
2022-07-05 11:19:29 +0530

    hello world

---
+++ social-media/src/components/Home/Home.js
@@ -0,0 +1,10 @@
+//import { useNavigate } from "react-router-dom";
+
+export default function Home(props){
+        
+    return(
+        <div>
+                <h1>Hello Project!!</h1>
+        </div>
+    );
+}
